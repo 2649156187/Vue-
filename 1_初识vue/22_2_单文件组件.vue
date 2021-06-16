7c0de43ce084b894f0213d6c6a183ccce7da774f
子龙 <2649156187@qq.com>
2021-06-16 09:53:49 +0800

    提交

---
+++ 1_初识vue/22_2_单文件组件.vue
@@ -0,0 +1,26 @@
+/* 配置模板结构 */
+<template>
+  <div>
+    <h3 @click="showInfo">{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name:'张三'
+    }
+  },
+  methods:{
+    showInfo() {
+      alert("测试信息")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
